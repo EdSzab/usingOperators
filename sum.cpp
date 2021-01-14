529e8e26616f9bb1d08886c6fd5d2738b26063ca
Eddie Sz <eddieszabatin@gmail.com>
2021-01-14 15:33:51 -0800

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    int sum;
+    cout << "Enter 2 numbers that you want to add together. \n Type a number: ";
+    cin >> x;
+    cout << "Type another number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "Sum is: " << sum;
+    return 0;
+}
